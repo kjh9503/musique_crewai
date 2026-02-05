fa9abd9ba13a25e0f6557f1ed9828c93d7d4cf95
kimjeonghoon <90254195+kjh9503@users.noreply.github.com>
2026-02-05 14:11:37 +0900

    Create process.py

---
+++ data/process.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+
+num=1
+def main() -> None:
+    input_path = Path("./musique_full_v1.0_train.jsonl")
+    items = []
+    with input_path.open("r", encoding="utf-8") as f:
+        for line in f:
+            if line.strip():
+                items.append(json.loads(line))
+    
+    for i in range(100):
+        output_path = Path(f"./musique_train_{i}.json")
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        
+        with output_path.open("w", encoding="utf-8") as f:
+            json.dump(items[num*i:num*(i+1)], f, ensure_ascii=True, indent=2)
+
+
+if __name__ == "__main__":
+    main()
